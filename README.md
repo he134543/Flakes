e0c94b3854742293f6e597d2aacabff9f83399ca
Xinchen He <xinchenhe@umass.edu>
2024-08-13 14:40:16 -0400

    Exactextract python binding

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# Flakes
-A collection of nix flake files for recreating software environments
+# Nix and Flake
+Nix is a package manager that can create an isolated environment for running specific version of software.
+It is similar to conda, but Nix is more stable especially for dealing with non-python softwares.
+
+The `Flake.nix` is a control file to guide nix to create a virtual environmental, similar to the `environment.yml` file for `conda`.
+This repository saves flake files that I've created for different uses.
+
+
+# Quick Start
+
+Take the `exactextract_python`, a working environment that contains a python binding of [exactextract](https://github.com/isciences/exactextract) as an example.
+
+1. Install Nix and Enable Flake
+Follow the official guide to install Nix in your Linux system.
+Open the file `~/.config/nix/nix.conf` or `/etc/nix/nix.conf` with your text editor, add this line to the `nix.conf` file.
+```
+experimental-features = nix-command flakes
+```
+
+2. Develop your nix environment
+Note: deactivate the `conda` environment first if it is automatically hooked to your terminal.
+```{bash}
+conda deactivate
+cd exactextract_python
+nix develop
+```
+
+3. Test if the `exactextract` python binding is successfully installed
+```{bash}
+python
+...
+>>> import exactextract
+```
---
+++ exactextract_python/flake.nix
@@ -0,0 +1,79 @@
+{
+  description = "Flake for exactextract environment";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.05";
+  };
+
+  outputs = {self, nixpkgs}@inputs:
+
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in {
+
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        name = "exactextract";
+
+        venvDir = "./.venv";
+
+        buildInputs = [
+          pkgs.python311Packages.python
+          pkgs.python311Packages.venvShellHook
+          pkgs.python311Packages.xarray
+          pkgs.python311Packages.netcdf4
+          pkgs.python311Packages.dask
+          pkgs.python311Packages.scipy
+          pkgs.python311Packages.ipython
+          pkgs.python311Packages.matplotlib
+          pkgs.python311Packages.seaborn
+          pkgs.python311Packages.numpy
+          pkgs.python311Packages.pandas
+          pkgs.python311Packages.shapely
+          pkgs.python311Packages.tkinter
+          pkgs.python311Packages.geopandas
+          pkgs.python311Packages.rasterio
+          pkgs.python311Packages.holoviews
+          pkgs.python311Packages.pyproj
+          pkgs.python311Packages.joblib
+          pkgs.python311Packages.pillow
+          pkgs.python311Packages.scikit-learn
+          pkgs.python311Packages.six
+          pkgs.python311Packages.fiona
+          pkgs.python311Packages.shapely
+          pkgs.python311Packages.geopandas
+          pkgs.python311Packages.gdal
+          pkgs.python311Packages.tqdm
+          # development packages
+          pkgs.python311Packages.python-lsp-server
+          pkgs.python311Packages.pyflakes
+          pkgs.python311Packages.yapf
+          pkgs.python311Packages.pycodestyle
+          pkgs.python311Packages.pydocstyle
+          pkgs.python311Packages.pybind11
+          pkgs.python311Packages.ipykernel
+          # non-python packages
+          pkgs.pandoc
+          pkgs.nco
+          pkgs.libgcc
+          pkgs.jupyter
+          pkgs.cmake
+          pkgs.gdal
+          pkgs.geos
+        ];
+
+        postVenvCreation = ''
+          unset SOURCE_DATE_EPOCH
+          pip install --upgrade pip
+          pip install -U p_tqdm
+          cd exactextract
+          pip install .
+          cd ..
+        '';
+
+        postShellHook = ''
+          unset SOURCE_DATE_EPOCH
+        '';
+      };
+
+    };
+}
